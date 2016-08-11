d17b7dcd6721a23c108c1f7eac3dcec33815ab74
Harris Kristanto <mr.kristanto@gmail.com>
2016-08-11 15:05:42 +1200

    first commit

--- My Task List/SecondViewController.swift
+++ My Task List/SecondViewController.swift
@@ -1,15 +1,10 @@
-//
-//  SecondViewController.swift
-//  My Task List
-//
-//  Created by Harris Kristanto on 8/11/16.
-//  Copyright © 2016 Harris Kristanto. All rights reserved.
-//
-
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var txtTask : UITextField!
+    @IBOutlet var txtDesc : UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +14,33 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Events
+    @IBAction func btnAddTask_Click(sender: UIButton){
+        taskMgr.addTask(txtTask.text!, desc:txtDesc.text!)
+        self.view.endEditing(true)
+        
+        //Clears up the text boxes for user
+        txtTask.text = ""
+        txtDesc.text = ""
+        
+        //Jumps user back to first view(list of tasks)
+        self.tabBarController?.selectedIndex = 0;
+        
+    }
+    
+    
+    //iOS Touch Function
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
+    //UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
